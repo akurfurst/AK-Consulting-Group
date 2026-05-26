7267ef88dda532939229da46bb15742e875753e9
akurfurst <19035144+akurfurst@users.noreply.github.com>
2026-05-26 13:55:43 -0700

    Added products page with simple cards

--- AK-Office_Supplies/src/controllers/ak.office.controller.js
+++ AK-Office_Supplies/src/controllers/ak.office.controller.js
@@ -1,8 +1,8 @@
-import { featuredProducts } from '../services/ak.office.service.js';
+import * as services from '../services/ak.office.service.js';
 
 export const getLanding = async (req, res) => {
     try {
-        const featured = await featuredProducts();
+        const featured = await services.featuredProducts();
         res.status(200).render('index', { title: 'Home', featured });
     } catch (err) {
         console.error('Error loading landing page:', err);
@@ -18,8 +18,9 @@ export const getRegister = (req, res) => {
     res.render('register', { title: 'Register' });
 };
 
-export const getProducts = (req, res) => {
-    res.render('products', { title: 'Products' });
+export const getProducts = async (req, res) => {
+    const products = await services.allProducts();
+    res.render('products', { title: 'Products', products });
 };
 export const getSuccess = (req, res) => {
     res.status(200).render('200', { title: 'Success' });
