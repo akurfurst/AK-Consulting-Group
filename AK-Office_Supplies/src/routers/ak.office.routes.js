445f699a9fc82be8f79ff9adf462cc94991cf245
kelley <kelleycastillo@outlook.com>
2026-06-02 08:59:20 -0700

    updated router to get product by id

--- AK-Office_Supplies/src/routers/ak.office.routes.js
+++ AK-Office_Supplies/src/routers/ak.office.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getLanding, getLogin, getRegister, getProducts, getSuccess, getNotFound } from '../controllers/ak.office.controller.js';
+import { getLanding, getLogin, getRegister, getProducts, getProductDetail, getSuccess, getNotFound } from '../controllers/ak.office.controller.js';
 
 const router = Router();
 
@@ -7,7 +7,8 @@ router.get('/', getLanding);
 router.get('/login', getLogin);
 router.get('/register', getRegister);
 router.get('/products', getProducts);
+router.get('/products/:id', getProductDetail);
 router.get('/success', getSuccess);
-router.use(getNotFound); // must be last
+router.use(getNotFound); 
 
 export default router;
